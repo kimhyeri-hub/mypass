43e7a09914278ae2fc1a1d29e083e592ca6e6621
oh <o5jongha312@gmail.com>
2026-09-15 15:23:20 +0900

    버그 픽스

--- backend/src/test/java/com/interview/backend/security/TestSecurityConfig.java
+++ backend/src/test/java/com/interview/backend/security/TestSecurityConfig.java
@@ -1,14 +1,14 @@
 package com.interview.backend.security;
 
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+@TestConfiguration
 @EnableWebSecurity
 public class TestSecurityConfig {
 
